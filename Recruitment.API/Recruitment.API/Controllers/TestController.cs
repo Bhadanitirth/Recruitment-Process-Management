d08ca281573d5d429fd446ee902d024d0904b9a9
DDU Student <DDU@Student>
2025-08-26 20:53:20 +0530

    feat: Backend connected to database and test API built successfully

---
+++ Recruitment.API/Recruitment.API/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Recruitment.API.Data;
+
+namespace Recruitment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("roles")]
+        public async Task<IActionResult> GetRoles()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            return Ok(roles); 
+        }
+    }
+}
---
+++ Recruitment.API/Recruitment.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recruitment.API.Models; 
+
+namespace Recruitment.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
